153bf8e1d2db3d594df0d14b70abb9835d83a8f1
Nikita <lifesteel6@gmail.com>
2023-10-28 14:34:31 +0400

    1. Внес в ресурсы пропущенный размер. 2. Исправил названия переменных. 3. Немного изменил размеры

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -9,19 +9,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val clickButton = findViewById<Button>(R.id.buttonSearch)
-        val clickButton2 = findViewById<Button>(R.id.buttonMedia)
-        val clickButton3 = findViewById<Button>(R.id.buttonSetting)
+        val searchButton = findViewById<Button>(R.id.buttonSearch)
+        val mediaButton = findViewById<Button>(R.id.buttonMedia)
+        val settingButton = findViewById<Button>(R.id.buttonSetting)
 
-        clickButton.setOnClickListener  {
+        searchButton.setOnClickListener  {
             val displaySearch = Intent(this, SearchActivity::class.java)
             startActivity(displaySearch)
         }
-        clickButton2.setOnClickListener  {
+        mediaButton.setOnClickListener  {
             val displayMedia = Intent(this, MediatekaActivity::class.java)
             startActivity(displayMedia)
         }
-        clickButton3.setOnClickListener  {
+        settingButton.setOnClickListener  {
             val displaySettings = Intent(this, SettingsActivity::class.java)
             startActivity(displaySettings)
         }
